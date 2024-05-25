3b6dbcf121538dc250ecd28495f6d4a8547447e5
Kevin Kroon <kevinjankroon@gmail.com>
2024-05-25 02:20:46 +0200

    plans for the comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include "../include/stuff.hpp"
 #include "../include/parser.hpp"
 
-int main(int argc, char **argv)
-{
-	if (argc != 1 && argc != 2)
-	{
+int main(int argc, char **argv) {
+	if (argc != 1 && argc != 2) {
 		std::cerr << "max 1 config file allowed" << std::endl;
 		return 1;
 	}
 	std::string config_file = "default.conf";
-	if (argc == 2)
-	{
+	if (argc == 2) {
 		config_file = argv[1];
 	}
 	Parser parser(config_file);
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,27 +1,62 @@
 #include "../include/stuff.hpp"
 #include "../include/parser.hpp"
 
-Parser::Parser(std::string config_file) : _config_file(config_file)
-{
+Parser::Parser(std::string config_file) : _config_file(config_file) {}
+
+Parser::~Parser() {}
+
+/*
+loop to start of the key
+loop to end of the key
+loop to start of the value
+loop to end of the value
+repeat last two steps until end of line
+
+
+
+*/
+void parse_line(std::string &line) {
+	
+	
+	
 }
 
-Parser::~Parser()
-{
-}
-
-void Parser::parse()
-{
+void Parser::parse() {
 	std::ifstream file(_config_file.c_str());
-	if (!file.is_open())
-	{
+	if (!file.is_open()) {
 		std::cerr << "could not open file" << std::endl;
 		return;
 	}
 	std::string line;
-	while (std::getline(file, line))
-	{
-		// parse_line(line);
+	while (std::getline(file, line)) {
+		// std::cout << line << std::endl;
+		parse_line(line);
 		std::cout << line << std::endl;
 	}
 }
+
+/*
+removing whitespace
+end of the line is semi colon or { or }
+some things are mandatory
+some things are optional
+some things are repeated
+some things are nested
+some things are not
+some things are in a specific order
+some things are not
+*/
+/*
+spaces in front
+splitting on spaces
+array of things that can be repeated on same line
+and array of things that can be repeated on multiple lines
+
+
+so an array of locations
+each location is:
+- root
+- index
+- autoindex
+*/
 
