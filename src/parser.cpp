8dd377e25b0b94807be8241a03ec6922154d2a04
Kevin Kroon <kevinjankroon@gmail.com>
2024-05-25 22:07:04 +0200

    raw server blocks

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,6 +4,37 @@ Parser::Parser(string config_file) : _config_file(config_file) {}
 
 Parser::~Parser() {}
 
+void extract_raw_servers(vector<vector<string>> &raw_config, vector<vector<vector<string>>> &servers) {
+	vector<vector<string>> server;
+	for (int i = 0; i < raw_config.size(); i++) {
+		if (raw_config[i][0] == "server") {
+			if (server.size() > 0) {
+				servers.push_back(server);
+				server.clear();
+			}
+		}
+		server.push_back(raw_config[i]);
+	}
+	if (server.size() > 0) {
+		servers.push_back(server);
+	}
+}
+
+void print_raw_servers(vector<vector<vector<string>>> &servers) {
+	for (int i = 0; i < servers.size(); i++) {
+		cout << "server block " << i << endl;
+		for (int j = 0; j < servers[i].size(); j++) {
+			for (int k = 0; k < servers[i][j].size(); k++) {
+				cout << servers[i][j][k];
+				if (k < servers[i][j].size() - 1) {
+					cout << "|";
+				}
+			}
+			cout << endl;
+		}
+	}
+}
+
 void Parser::parse() {
 	ifstream file(_config_file.c_str());
 	if (!file.is_open()) {
@@ -12,8 +43,12 @@ void Parser::parse() {
 	}
 	vector<vector<string>> raw_config;
 	raw_extract(file, raw_config);
-	print_raw_config(raw_config);
+	// print_raw_config(raw_config);
+	vector<vector<vector<string>>> servers;
+	extract_raw_servers(raw_config, servers);
+	// print_raw_servers(servers);
 	//check brackets are closed correctly
+	//splitting on server blocks
 }
 /*
 can have multiple server blocks
