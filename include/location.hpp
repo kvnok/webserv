d12bd32a5cf4d80e933c25d8dfe71666673f270d
Kevin Kroon <kevinjankroon@gmail.com>
2024-05-29 11:50:39 +0200

    no body limit in location for now atleast

--- include/location.hpp
+++ include/location.hpp
@@ -7,20 +7,18 @@ class Location {
 		string _root;
 		string _index;
 		string _autoindex;
-		string _body_limit;
 		bool _is_cgi;
 		string _cgi_extension;
 		bool _is_redirect;
 		string _redirect;
 	public:
 		Location();
-		Location(string path, string root, string index, string autoindex, string body_limit, bool is_cgi, string cgi_extension, bool is_redirect, string redirect);
+		Location(string path, string root, string index, string autoindex, bool is_cgi, string cgi_extension, bool is_redirect, string redirect);
 		~Location();
 		string get_path() const;
 		string get_root() const;
 		string get_index() const;
 		string get_autoindex() const;
-		string get_body_limit() const;
 		bool get_is_cgi() const;
 		string get_cgi_extension() const;
 		bool get_is_redirect() const;
@@ -29,7 +27,6 @@ class Location {
 		void set_root(string root);
 		void set_index(string index);
 		void set_autoindex(string autoindex);
-		void set_body_limit(string body_limit);
 		void set_is_cgi(bool is_cgi);
 		void set_cgi_extension(string cgi_extension);
 		void set_is_redirect(bool is_redirect);
