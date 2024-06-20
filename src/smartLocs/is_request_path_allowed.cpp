e2af96167bc5c2d47d970a643f65d62ecaa27686
Kevin Kroon <kkroon@student.codam.nl>
2024-06-20 18:33:42 +0200

    thinking about what kind of paths from the request we could get and how to interpret them

--- src/smartLocs/is_request_path_allowed.cpp
+++ src/smartLocs/is_request_path_allowed.cpp
@@ -1,12 +1,27 @@
 #include "Connection.hpp"
 
 
+// set path to the relative path to the file or the correct error page
+// if the path is not allowed, set the status code to 403
+// if the path is not found, set the status code to 404
+
+// check if they are asking for a file or a folder
+
 void request_path_handler(string &path, Request &request) {
-	// set path to the relative path to the file or the correct error page
-	// if the path is not allowed, set the status code to 403
-	// if the path is not found, set the status code to 404
+	// first check if its just / or /file or /folder/
+
+	
 	
 	smartLocs sLocs;
 	sLocs.set_locs(request.getServer().getSmartLocs().get_locs());
-	path = "www" + request.getPath();
+	// path = "www" + request.getPath();
+	// get only the dir path
 }
+
+/*
+/www/index.html -> /www
+/www/ -> /www
+/ -> /
+/bla/bla/bla -> /bla/bla
+/test/one.html -> /test
+*/
