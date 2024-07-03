e6e9fc5bd8b617aba3fb32d1f2de96770b8d6895
Kevin Kroon <kkroon@student.codam.nl>
2024-07-03 15:46:44 +0200

    printing

--- src/smartLocs/test.cpp
+++ src/smartLocs/test.cpp
@@ -4,6 +4,7 @@ void test_smartLocs(Config &config) {
 	vector<pServerBlock> server_blocks = config.get_server_blocks();
 	for (int i = 0; i < server_blocks.size(); i++)
 	{
+		cout << RED << "Testing smartLocs" << RESET << endl;
 		color_print(GRN, "pServerBlock: ", i);
 		pServerBlock server_block = server_blocks[i];
 		smartLocs smartLocs(server_block);
